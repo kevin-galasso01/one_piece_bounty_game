a5b4befcf845b0b05cdf32e1d793d950a3cb4ccb
kevin <kevin.galasso01@gmail.com>
2022-01-21 20:19:29 +0100

    V0.1.1 Aggiunta dell'immagine divisoria (ossa al centro)

--- src/components/character_box/CharacterBox.js
+++ src/components/character_box/CharacterBox.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import "../character_box/character_box_style.css";
 import Character from './character/Character';
+import DividingImg from "./dividing_img/DividingImg";
 
 const CharacterBox = () => {
     return (
         <div className='character_box'>
             <Character />
             <Character />
+            <DividingImg />
         </div>
     );
 };
---
+++ src/components/character_box/dividing_img/DividingImg.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Dividing_image from '../../../images/Dividing_image.png';
+import '../dividing_img/dividing_img_style.css'
+
+const DividingImg = () => {
+    return (
+        <img className='bones_img' src={Dividing_image} alt='' />
+    );
+};
+
+export default DividingImg;
